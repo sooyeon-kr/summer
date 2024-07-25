5f9e34f964d99a90a8861e5186ea18eb672f46db
sooyeon-kr <sooyeon7027@naver.com>
2024-07-25 11:16:06 +0900

    Feat: 로그 추가

--- summer/summer/src/main/java/com/example/summer/exception/GlobalExceptionHandler.java
+++ summer/summer/src/main/java/com/example/summer/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.example.summer.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class GlobalExceptionHandler {
 
+//    로거를 새성할 수 있는 곳이 팩토리
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(value = RoomNotFoundException.class)
     public String catchRoomNotFoundException(RoomNotFoundException e){
+        logger.error("exception class: {}", e.getClass());
+        logger.error("exception class: " , e);
         return "No Room";
     }
 
