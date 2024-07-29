4f29708a63cea9db24b959c101118cd44aaf25c9
sooyeon-kr <sooyeon7027@naver.com>
2024-07-29 15:59:14 +0900

    Update: Primary 어노테이션 추가

--- summer/summer/src/main/java/com/example/summer/room/RoomSpringDataJPARepository.java
+++ summer/summer/src/main/java/com/example/summer/room/RoomSpringDataJPARepository.java
@@ -1,7 +1,10 @@
 package com.example.summer.room;
 
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-public interface RoomSpringDataJPARepository extends JpaRepository<Room, Integer> {
+@Primary
+public interface RoomSpringDataJPARepository extends JpaRepository<Room, Integer>, RoomRepository {
 }
