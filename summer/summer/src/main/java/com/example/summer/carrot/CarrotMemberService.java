f848586dd09fea11d8d10cffea87afe9ad6c2463
sooyeon-kr <sooyeon7027@naver.com>
2024-07-31 13:53:29 +0900

    Update: Policy를 모두 DiscountPolicy 타입으로 변경하고, 회원 정책을 Qualifier로 설정

--- summer/summer/src/main/java/com/example/summer/carrot/CarrotMemberService.java
+++ summer/summer/src/main/java/com/example/summer/carrot/CarrotMemberService.java
@@ -1,13 +1,14 @@
 package com.example.summer.carrot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarrotMemberService extends CarrotService{
 
     @Autowired
-    public CarrotMemberService(DiscountPolicy discountPolicy){
+    public CarrotMemberService(@Qualifier("discountMemberPolicy")DiscountPolicy discountPolicy){
         super(discountPolicy);
     }
 
