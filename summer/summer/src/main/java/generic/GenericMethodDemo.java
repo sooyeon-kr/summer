412d7615ebdedccdc8e222e31fe23b80cac334bf
sooyeon-kr <sooyeon7027@naver.com>
2024-08-02 11:38:51 +0900

    Add: 클래스 메소드

---
+++ summer/summer/src/main/java/generic/GenericMethodDemo.java
@@ -0,0 +1,20 @@
+package generic;
+
+public class GenericMethodDemo {
+    public static void main(String[] args) {
+
+    }
+}
+class GenericClass<C>{
+    C c;
+
+//    클래스제네릭
+    public void methodC(C c){
+
+    }
+}
+class ClassWithGenericMethod<T> {
+    T t;
+
+}
+
